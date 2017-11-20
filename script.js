6f85e2c2a5479d42d9009f92b9a6698e0c610531
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2017-11-20 12:24:00 -0800

    hello

--- script.js
+++ script.js
@@ -4,25 +4,16 @@ var word="";
 var difficult= ["COMPUTER","DECEMBER","DOCUMENT"];
 var slightlyDifficult=["WINTER","GEMINI","SUMMER","ENGLISH"];
     var easy= ["MATH","HELLO","FRENCH" ];
-var guesses=15;
+var guesses=10;
 var guessedLetters=[];
 var letter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R", "S", "T", "U","V","W","X","Y","Z"];
 
 function startGame(){
-    guesses=15;
+    guesses=10;
     guessedLetters=[];
+    // levelOfDifficulty();
 //get value out of select box and set into a var
-//    var diff = document.getElementById("difficulty").innerHTML=word;
-//
-//         if(difficulty=1) {
-//         word = difficult[Math.floor(Math.random() * difficult.length)];
-//     }
-//     if(dif=2){
-//         word = slightlyDifficult[Math.floor(Math.random() * slightlyDifficult.length)];
-//     }
-//     if(dif=3){
-//         word = easy[Math.floor(Math.random() * easy.length)];
-//     }
+
 
     var rand = difficult[Math.floor(Math.random() * difficult.length)];
     var rand1=slightlyDifficult[Math.floor(Math.random() * slightlyDifficult.length)];
@@ -33,6 +24,7 @@ function startGame(){
     word=rand1;
     word=rand2;
     printWord();
+
 }
 
 function printWord(){
@@ -44,9 +36,8 @@ function printWord(){
     }else{
         ret+="_ ";
     }
-document.getElementById("output").innerHTML=ret;
-    document.getElementById("guess").innerHTML="remaining guesses:" + guesses
-
+document.getElementById("output1").innerHTML=ret;
+    document.getElementById("guess").innerHTML="Remaining Guesses:" + guesses
 }
 console.log(ret);
 }
@@ -60,6 +51,7 @@ function letters(userGuess){
 
     if(guesses<0){
         return document.getElementById("lose").innerHTML="You lost, Try again next time!";
+    }
     }
     printWord();
 
@@ -82,3 +74,37 @@ function letters(userGuess){
 //
 //     }
 //     if()
+
+
+function populateLetters() {
+    var r="";
+    for (var i = 0; i < letter; i++) {
+        if (guessedLetters.indexOf(letter[i]) > -1) {
+          r+= "<button class='submit' onclick='letters(this.value)' value='" + letter[i] + "'>" + letter[i] + "</button>"
+
+        } else {
+
+           r+= "<button class='submit' onclick='letters(this.value)' value='" + letter[i] + "' disabled>" + letter[i] + "</button>"
+        }
+//if letter we are on in iteration is in guessed
+
+        // "<button class='submit' onclick='letters(this.value)' value='" + letter[i] + "' disabled>" + letter[i] + "</button>"
+
+    }
+}
+// function levelOfDifficulty(){
+//     var diff = document.getElementById("difficulty").innerHTML=word;
+//
+//     if(difficulty=1) {
+//         word = difficult[Math.floor(Math.random() * difficult.length)];
+//     }
+//     console.log(word);
+//     if(dif=2){
+//         word = slightlyDifficult[Math.floor(Math.random() * slightlyDifficult.length)];
+//     }
+//     console.log(word);
+//     if(dif=3){
+//         word = easy[Math.floor(Math.random() * easy.length)];
+//     }
+//     console.log(word);
+// }
